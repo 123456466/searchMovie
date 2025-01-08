f2d90a8ea3e8aeb94da8b2dc03f59f65f30e9ee8
unknown <sunji7378@gmail.com>
2025-01-08 20:36:36 +0900

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const options = {
     }
 };
 
-fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc', options)
+fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=2&sort_by=popularity.desc', options)
     .then(res => res.json())
     .then(res => console.log(res))
     .catch(err => console.error(err));
